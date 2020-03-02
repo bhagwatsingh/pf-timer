8571f55c83e4a28c7e208d0de8c9b48daccbb5df
Bhagwat Singh <er.bhagwatsingh@gmail.com>
2020-03-02 16:39:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Plugin Name: PF Timer Plugin
+ * Plugin URI: https://plugins.profit-funnels.in/
+ * Description: Countdown Timer Plugin
+ * Version: 1.0
+ * Author: Team Profit-Funnels
+ * Author URI: https://profit-funnels.in
+ */
+
+if ( ! defined( 'PF_TIMER_VERSION' ) ) {
+	define( 'PF_TIMER_VERSION', '1.0.0' ); // Version of plugin
+}
+if ( ! defined( 'PF_TIMER_DIR' ) ) {
+	define( 'PF_TIMER_DIR', dirname( __FILE__ ) ); // Plugin dir
+}
+if ( ! defined( 'PF_TIMER_BASE_FILE' ) ) {
+	define( 'PF_TIMER_BASE_FILE', __FILE__ ); // Plugin dir
+}
+if ( ! defined( 'PF_TIMER_URL' ) ) {
+	define( 'PF_TIMER_URL', plugin_dir_url( __FILE__ ) ); // Plugin url
+}
+if ( ! defined( 'PF_TIMER_PLUGIN_BASENAME' ) ) {
+	define( 'PF_TIMER_PLUGIN_BASENAME', plugin_basename( __FILE__ ) ); // plugin base name
+}
+if ( ! defined( 'PF_TIMER_POST_TYPE' ) ) {
+	define( 'PF_TIMER_POST_TYPE', 'pf_timer' ); // Plugin post type
+}
+if ( ! defined( 'PF_TIMER_META_PREFIX' ) ) {
+	define( 'PF_TIMER_META_PREFIX', '_pftimer_' ); // Plugin metabox prefix
+}
+
+/**
+ * Activation Hook
+ *
+ * Register plugin activation hook.
+ *
+ * @package PF Timer
+ * @since 1.0.0
+ */
+register_activation_hook( PF_TIMER_BASE_FILE, 'pf_timer_install' );
+
+/**
+ * Deactivation Hook
+ *
+ * Register plugin deactivation hook.
+ *
+ * @package PF Timer
+ * @since 1.0.0
+ */
+register_deactivation_hook( PF_TIMER_BASE_FILE, 'pf_timer_uninstall' );
+
+
+/**
+ * Plugin Setup (On Activation)
+ *
+ * Does the initial setup,
+ * stest default values for the plugin options.
+ *
+ * @package PF Timer
+ * @since 1.0.0
+ */
+function pf_timer_install() {
+
+	// IMP need to flush rules for custom registered post type
+	flush_rewrite_rules();
+}
+
+/**
+ * Plugin Setup (On Deactivation)
+ *
+ * Delete plugin options.
+ *
+ * @package PF Timer
+ * @since 1.0.0
+ */
+
+function pf_timer_uninstall() {
+
+	// IMP need to flush rules for custom registered post type
+	flush_rewrite_rules();
+}
+
+require_once( PF_TIMER_DIR . '/includes/pf_timer_class.php' );
+
+if ( is_admin() ) {
+	$pf_timer = new PFTimer();
+}
