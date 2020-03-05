64c2fe15f5526df2e5550f75561181fa188bd61b
Bhagwat Singh <er.bhagwatsingh@gmail.com>
2020-03-05 14:57:16 +0530

    phpcs error fixes.

--- assets/js/pf-script.js
+++ assets/js/pf-script.js
@@ -1,22 +1,36 @@
-jQuery(document).ready(function ($) {
+/**
+ * PFTimer admin script
+ *
+ * @category JS
+ * @package PFTimer
+ * @since 1.0.0
+ * @subpackage PFTimer/assets/js
+ * @author Team Profit-Funnels
+ */
 
-    $('.pf_timer_expiry_date').datetimepicker({
-        dateFormat: 'yy-mm-dd',
-        timeFormat: 'HH:mm:ss',
-        minDate: 0,
-        changeMonth: true,
-        changeYear: true,
-    });
+jQuery( document ).ready(
+	function ($) {
 
-    $('.pf_timer_bg_color,.pf_timer_font_color').wpColorPicker();
+		$( '.pf_timer_expiry_date' ).datetimepicker(
+			{
+				dateFormat: 'yy-mm-dd',
+				timeFormat: 'HH:mm:ss',
+				minDate: 0,
+				changeMonth: true,
+				changeYear: true,
+			}
+		);
 
-    var pf_timer_font_size = document.getElementById("pf_timer_font_size");
-    var pf_timer_border_radius = document.getElementById("pf_timer_border_radius");
+		$( '.pf_timer_bg_color,.pf_timer_font_color' ).wpColorPicker();
 
-    pf_timer_font_size.oninput = function () {
-        document.getElementById("pf_timer_font_size_span").innerHTML = this.value + "px";
-    }
-    pf_timer_border_radius.oninput = function () {
-        document.getElementById("pf_timer_border_radius_span").innerHTML = this.value + "px";
-    }
-});
+		var pf_timer_font_size     = document.getElementById( "pf_timer_font_size" );
+		var pf_timer_border_radius = document.getElementById( "pf_timer_border_radius" );
+
+		pf_timer_font_size.oninput     = function () {
+			document.getElementById( "pf_timer_font_size_span" ).innerHTML = this.value + "px";
+		}
+		pf_timer_border_radius.oninput = function () {
+			document.getElementById( "pf_timer_border_radius_span" ).innerHTML = this.value + "px";
+		}
+	}
+);
